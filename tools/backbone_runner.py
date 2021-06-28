ebda661dc23d93028298c90700bb681735f5544a
aminst <aminst79@gmail.com>
2021-06-28 16:03:22 +0430

    Add inflation_rate to backbone_runner

--- tools/backbone_runner.py
+++ tools/backbone_runner.py
@@ -59,6 +59,7 @@ def get_nodes_list(G: nx.DiGraph):
     data={}
     data["country_iso3"]=[x for x in G]
     data["gdp_us_dollar"] = [G.nodes[x]["gdp_us_dollar"] for x in G.nodes()]
+    data["inflation_rate"] = [G.nodes[x]["inflation_rate"] for x in G.nodes()]
     return pd.DataFrame(data)
 
 def main():
