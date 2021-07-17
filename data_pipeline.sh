10ad78b40e45a0befa6dac9cda92325528b25483
aminst <aminst79@gmail.com>
2021-07-17 12:58:34 +0430

    Update data_pipeline

--- data_pipeline.sh
+++ data_pipeline.sh
@@ -2,5 +2,6 @@ python tools/trade_cleaner.py data/Trade_BEH0_2011_Export_2020Jul10.csv tmp/trad
 python tools/feature_merger.py data/features/gdp_2011.csv data/features/inflation_2011.csv tmp/features.csv
 python tools/feature_merger.py tmp/features.csv data/features/gdp_growth_2011.csv tmp/features.csv
 python tools/feature_merger.py tmp/features.csv data/features/colonization.csv tmp/features.csv
+python tools/feature_merger.py tmp/features.csv data/features/geo_cepii.csv tmp/features.csv
 python tools/missing_handler.py tmp/features.csv tmp/trades.csv tmp/nl_no_missing.csv tmp/el_no_missing.csv
 python tools/backbone_runner.py tmp/nl_no_missing.csv tmp/el_no_missing.csv 0.8 1 data/nodelist_2011.csv data/edgelist_2011.csv reports
