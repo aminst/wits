f909b4285a131e866cf7883990778da3706697b1
aminst <aminst79@gmail.com>
2021-07-28 16:06:04 +0430

    Update ergm terms

--- data_pipeline.sh
+++ data_pipeline.sh
@@ -8,5 +8,7 @@ python tools/feature_merger.py tmp/features.csv data/features/population.csv tmp
 python tools/feature_merger.py tmp/features.csv data/features/gdp_per_capita.csv tmp/features.csv
 python tools/feature_merger.py tmp/features.csv data/features/life_expectancy.csv tmp/features.csv
 python tools/feature_merger.py tmp/features.csv data/features/gni_atlas.csv tmp/features.csv
+python tools/feature_merger.py tmp/features.csv data/features/agriculture_forestry_fishing_of_gdp.csv tmp/features.csv
+python tools/feature_merger.py tmp/features.csv data/features/industry_of_gdp.csv tmp/features.csv
 python tools/missing_handler.py tmp/features.csv tmp/trades.csv tmp/nl_no_missing.csv tmp/el_no_missing.csv
 python tools/backbone_runner.py tmp/nl_no_missing.csv tmp/el_no_missing.csv 0.8 1 data/nodelist_2011.csv data/edgelist_2011.csv reports
