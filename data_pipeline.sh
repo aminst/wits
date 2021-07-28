19168c40ca4182d6b7d547a5f84dfa0caa08a72e
aminst <aminst79@gmail.com>
2021-07-28 14:39:46 +0430

    Add new features

--- data_pipeline.sh
+++ data_pipeline.sh
@@ -5,5 +5,8 @@ python tools/feature_merger.py tmp/features.csv data/features/gdp_growth_2011.cs
 python tools/feature_merger.py tmp/features.csv data/features/colonization.csv tmp/features.csv
 python tools/feature_merger.py tmp/features.csv data/features/geo_cepii.csv tmp/features.csv
 python tools/feature_merger.py tmp/features.csv data/features/population.csv tmp/features.csv
+python tools/feature_merger.py tmp/features.csv data/features/gdp_per_capita.csv tmp/features.csv
+python tools/feature_merger.py tmp/features.csv data/features/life_expectancy.csv tmp/features.csv
+python tools/feature_merger.py tmp/features.csv data/features/gni_atlas.csv tmp/features.csv
 python tools/missing_handler.py tmp/features.csv tmp/trades.csv tmp/nl_no_missing.csv tmp/el_no_missing.csv
 python tools/backbone_runner.py tmp/nl_no_missing.csv tmp/el_no_missing.csv 0.8 1 data/nodelist_2011.csv data/edgelist_2011.csv reports
